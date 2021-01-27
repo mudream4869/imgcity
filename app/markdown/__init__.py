08635549269ed1a6ce535773306fdbd44a40ed57
Mudream <mudream4869@gmail.com>
2021-01-27 22:56:26 +0800

    Modify: fix del word, refine blog

--- app/markdown/__init__.py
+++ app/markdown/__init__.py
@@ -1,13 +1,23 @@
 import markdown
 from bs4 import BeautifulSoup
+from markdown.inlinepatterns import SimpleTagInlineProcessor
+from markdown.extensions import Extension
+
+
+class DelExtension(Extension):
+    def extendMarkdown(self, md):
+        md.inlinePatterns.register(
+            SimpleTagInlineProcessor(r'()~~(.*?)~~', 'del'), 'del', 175)
+
+
+markdown_extensions = ['tables', 'fenced_code', 'app.markdown:DelExtension']
 
 
 def markdownToHTML(content: str):
     """
         Transform markdown to HTML
     """
-    return markdown.markdown(
-        content, extensions=['tables', 'fenced_code'])
+    return markdown.markdown(content, extensions=markdown_extensions)
 
 
 def abbrMarkdownHTML(content: str, line_count=3):
